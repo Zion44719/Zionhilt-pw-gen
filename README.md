f8de2a6e06748873c1758c1b5163956df7957b51
Zion44719 <zionctech@gmail.com>
2025-11-24 08:40:43 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Zionhilt Password Generator
+
+The **Zionhilt Password Generator** application is a simple tool to generate secure password. This app allows you to generate secure passphrase password.
+
+
+## Installation
+
+1. Clone the repository or download the script.
+2. Run the script:
+
+
+
+## License
+
+This project is open-source and available under the MIT License.
+
---
+++ js/index.js
@@ -0,0 +1,28 @@
+// document.write("Your Safety My Safety, An Injury To One Is An Injury To All");
+// alert("Welcome To Zion Safety Cache")
+        
+// document.querySelector('#fullName')
+// document.querySelector('.fullName').value = "Akere"
+// document.querySelector('p')
+
+
+// function Hello() {
+//     let Hello=prompt('How Can We Serve Your Request?')
+//     alert('Stay Calm As We Will Get Back To You Shortly. Thanks')        
+    
+// }
+
+// document.querySelector('#reportForm').addEventListener('submit', registerParticipant)
+
+
+// function registerParticipant(event) {
+//     event.preventDefault();
+//     const fullName = document.querySelector('#fullName')
+//     const residentAddress = document.querySelector('#residentAddress')
+//     const emailAddress = document.querySelector('#emailAddress')
+//     const experienceAbout = document.querySelector('#experienceAbout')
+//     const securityChallenges = document.querySelector('#securityChallenges')
+//     const overCome = document.querySelector('#overCome')
+//     olDisplay.innerHTML = `${fullName.value}`
+    
+// }
